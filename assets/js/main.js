7f92b3bb99caf9c9996310147c462ea130c5335f
“Laura <laureen.pelaez.isaza@gmail.com>
2017-07-07 00:09:12 -0400

    updated ajax -name-id-and picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,45 @@
 $(document).ready(function () {
     getPokemons();
 });
-var API = 'http://pokeapi.co/api/v2/';
+const API = 'http://pokeapi.co/api/v2/';  //Constant Api
+
+// With this function I try to look up through de api images to give it an id  http://pokeapi.co/docsv2/#pokemonsprites
+
+function getPokemonImage(id) {
+    var url = "http://pokeapi.co/media/sprites/pokemon/";
+    return url + id + '.png';
+}
+
+// get all the pokemons from api with ajax.
+function getPokemons() {
+    $.ajax({
+        url: API + "pokemon?limit=20" //limit 20 because it is too slow to run the 811
+    }).done(function (response) {
+
+        for (var i = 0; i < response.results.length; i++) {
+            var name = response.results[i].name;
+            var url_id = response.results[i].url.split('/');//To get only the ID number
+            var id = url_id[url_id.length - 2];
+            var image = getPokemonImage(id);
+            console.log(image);
+            var html = `<div class="col s10 m2 l2">
+                <div class="pokemon-box">
+                    <figure class="photo"><a href="#modal1" data-id="`+ id + `"><img src="` + image + `" alt="photo"></a></figure>
+                    <div class="notch-collectibles-small">
+                        <div class="collectibles-wrapper">
+                            <div class="collectibles-collection"><a href="#" rel="tooltip"<span class="icon icon_collection"><img src="assets/icon/pokeball_gray.png" alt="icon"></span></a></div>
+                            <div class="collectibles-wishlist"><a href="#" rel="tooltip"<span class="icon icon_wishlist"><img src="assets/icon/valentines-heart.png" alt="icon"> </span></a></div>
+                            <div class="collectibles-trade"><a href="#" rel="tooltip"><span class="icon icon_trade"><img src="assets/icon/data.png" alt="icon"></span></a></div>
+                        </div>
+                        <div class="texto">
+                            <medium>`+ name + `</medium>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+            $('.pokemon-container').append(html);
+        }
+    });
+}
+

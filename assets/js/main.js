494a16dad6464af81ddbf681acae3d5732d0021b
“Laura <laureen.pelaez.isaza@gmail.com>
2017-07-08 12:34:39 -0400

    remove prevent default

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,11 +145,11 @@ function sortPokemons() {
 
 //ON KEY PRESS, LOOK UP FOR THE POKEMONS
 function onKeyPress(event) {
-    event.preventDefault();
     if (event.keyCode == 13) {
-        if ($('#new-task').val() != '') {
-            var result = $("#search").val();
+        var result = $("#search").val();
+        if (result != '') {         
             var objectPokemons = findPokemons(result);
+            console.log('object found: ');
             console.log(objectPokemons);
             if (typeof objectPokemons != "undefined") {
                 var url_id = objectPokemons.url.split('/');//To get only the ID number
